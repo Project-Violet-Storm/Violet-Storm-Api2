1df84be86489aad3b21df0e7858ded7b534ecb0b
Cooper Lillquist <lillquist.1@osu.edu>
2024-04-01 16:26:20 -0400

    Modified CatalogController

--- violet.storm.Api/Controllers/CatalogController.cs
+++ violet.storm.Api/Controllers/CatalogController.cs
@@ -7,7 +7,12 @@ namespace violet.storm.Api.Controller {
     public class CatalogController : ControllerBase {
         [HttpGet]
         public IActionResult GetItems(){
-            return Ok("Hello");
+            var items = new List<Item>(){
+                new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State Shorts", "Nike", 44.99m)
+            };
+            return Ok(items);
         }
+
     }
 }
