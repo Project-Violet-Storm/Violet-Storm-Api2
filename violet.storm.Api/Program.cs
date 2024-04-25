bf87ca1af86a29be1fede8e218c48dfc8290d3cc
Keith Webber <kcwebber02@gmail.com>
2024-04-24 23:11:39 -0400

    updated program.cs

--- violet.storm.Api/Program.cs
+++ violet.storm.Api/Program.cs
@@ -1,3 +1,6 @@
+using violet.storm.data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,14 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+/*
+builder.Services.AddDbContext<StoreContext>(options=> options.UseSqlite("Data Source = ../Registrar.sqlite", 
+    b => b.MigrationsAssembly("violet.storm.Api")));
+ */
+builder.Services.AddDbContext<StoreContext>(options=> 
+{
+    options.UseSqlServer(storeConnectionString, b => b.MigrationsAssembly("violet.storm.Api")); 
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+
+string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
+    throw new ArgumentNullException("ConnectionString:StoreConnection");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
-    throw new ArgumentNullException("ConnectionString:StoreConnection");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
